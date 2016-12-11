c1989d84cd05bfa401206c494fbbccc67eff2542
Liu Yummy <liuyummy@foxmail.com>
2016-12-11 22:40:25 +0800

    Java Project Created.

---
+++ src/gl/tl/NWSHospital.java
@@ -0,0 +1,10 @@
+package gl.tl;
+
+public class NWSHospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Project created on 2016/12/10.");
+	}
+
+}
